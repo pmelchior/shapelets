40ad4971f9d8cf195e5efc9ed9e269206b9a9189
Peter Melchior <peter.m.melchior@gmail.com>
2009-05-07 17:44:25 +0000

    Interpolation now really templated (for real and complex floating point types)

--- include/utils/Interpolation.h
+++ include/utils/Interpolation.h
@@ -12,10 +12,11 @@
 namespace shapelens {
   // helper class: uses a singleton for matrix w in bicubic
   // hence this matrix is only constructed once.
-  class BicubicW : public NumMatrix<data_t> {
+  template <class T>
+  class BicubicW : public NumMatrix<T> {
   public:
-  BicubicW() : NumMatrix<data_t>(16,16) {
-      data_t wt[256] = 
+  BicubicW() : NumMatrix<T>(16,16) {
+      T wt[256] = 
 	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	  0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,
 	  -3,0,0,3,0,0,0,0,-2,0,0,-1,0,0,0,0,
@@ -35,7 +36,7 @@ namespace shapelens {
       // copying this is not the fastest thing to do...
       // .. but we do it only once (w is static)
       for (int i = 0; i < 256; i++)
-	*(NumMatrix<data_t>::c_array()+i) = *(wt+i);
+	*(NumMatrix<T>::c_array()+i) = *(wt+i);
     }
   };
 
@@ -74,14 +75,14 @@ namespace shapelens {
     /// where \f$x_j\f$ are equidistantly spaced around \p x. In this case,
     /// \f$w_j=(-1)^j\binom{n}{j}\f$.
     template <class T>
-      static data_t polynomial(const NumVector<T>& data, data_t x, int n) {
+      static T polynomial(const NumVector<T>& data, data_t x, int n) {
       int x_ = (int) floor (x);
-      //std::cout << "1D x = " << x << "\t" << x_ << std::endl;
       if (x_ >= 0 && x_ < data.size()) {
 	if (fabs(x_ - x) < 1e-10) // if x is too close to sampling point
 	  return data(x_);        // use data from there instead
 	else {
-	  data_t x_j, k_j, num = 0, denom = 0;
+	  data_t x_j, k_j, denom = 0;
+	  T num(0);
 	  PolynomialW& w = Singleton<PolynomialW>::getInstance();
 	  w.setOrder(n);
 	  for (int j = 0; j <= n; j++) {
@@ -104,7 +105,7 @@ namespace shapelens {
     /// Analogous to equidistant sampling apart from
     /// \f[ w_j = \frac{1}{\Pi_{k\neq j} (x_j-x_k)} \f].
     template <class T>
-      static data_t polynomial(const NumVector<T>& data, const NumVector<data_t>& xx, data_t x, int n) {
+      static T polynomial(const NumVector<T>& data, const NumVector<data_t>& xx, data_t x, int n) {
       if (xx.size() != data.size())
 	throw std::invalid_argument("Interpolation: data and xx have different size");
       if (x >= xx.min() && x <= xx.max()) {
@@ -121,7 +122,8 @@ namespace shapelens {
 	if (index < n/2 || data.size() - index < n/2)
 	  throw std::invalid_argument("Interpolation: x is too close to boundary");
 	else {
-	  data_t num = 0, denom = 0, k_j = 1; // k_j = w_j/(x-x_j);
+	  data_t denom = 0, k_j = 1; // k_j = w_j/(x-x_j);
+	  T num(0);
 	  for (int j = 0; j <= n; j++) {
 	    for (int k = 0; k <= n; k++)
 	      if (k != j)
@@ -139,10 +141,9 @@ namespace shapelens {
     /// 2D polynomial interpolation of order \p n of \p im at <tt>(x,y)</tt>.
     /// Uses a series of 1D interpolations of order \p n.
     template <class T>
-      static data_t polynomial(const Image<T>& im, data_t x, data_t y, int n) {
+      static T polynomial(const Image<T>& im, data_t x, data_t y, int n) {
       if (x >= 0 && x < im.getSize(0) && y >= 0 && y < im.getSize(1)) {
-	NumVector<data_t> first_run(n+1);
-	NumVector<T> row(n+1);
+	NumVector<T> first_run(n+1),row(n+1);
 	int x_ = (int) floor(x);
 	int y_ = (int) floor(y);
 	for (int i=0; i <= n; i++) {
@@ -158,14 +159,14 @@ namespace shapelens {
     /// Bi-cubic interpolation of \p im at at <tt>(x,y)</tt>.
     /// Uses Numerical Recipes \p bcucof and finite differences for gradients
     template <class T>
-      static data_t bicubic(const Image<T>& im, data_t x, data_t y) {
+      static T bicubic(const Image<T>& im, data_t x, data_t y) {
       const Grid& grid = im.grid;
       if (x >=  grid.getStartPosition(0) && x < grid.getStopPosition(0) && y >= grid.getStartPosition(1) && y < grid.getStopPosition(1)) {
 	int x_ = (int) floor(x);
 	int y_ = (int) floor(y);
-	NumMatrix<data_t>& w = Singleton<BicubicW>::getInstance();
+	NumMatrix<T>& w = Singleton<BicubicW<T> >::getInstance();
 
-	NumVector<data_t> yy(4), y1(4), y2(4), y12(4);
+	NumVector<T> yy(4), y1(4), y2(4), y12(4);
 	for (int i=1; i < 5; i++) {
 	  int x__, y__;
 	  switch (i) { // numering scheme from Fig. 3.6.1
@@ -181,16 +182,16 @@ namespace shapelens {
 	  y12(i-1) = 0.25*(im.get(x__+1,y__+1) - im.get(x__+1,y__-1) - 
 			   im.get(x__-1,y__+1) - im.get(x__-1,y__-1));
 	}
-	NumVector<data_t> xx(16);
+	NumVector<T> xx(16);
 	for (int i=0; i < 4; i++) {
 	  xx(i)=yy(i);
 	  xx(i+4)=y1(i);
 	  xx(i+8)=y2(i);
 	  xx(i+12)=y12(i);
 	}
-	NumVector<data_t> cl = w*xx;
+	NumVector<T> cl = w*xx;
 	int l=0;
-	data_t p=0;
+	T p=0;
 	for (int i=0; i < 4; i++) {
 	  for (int j=0; j < 4; j++) {
 	    // cl(l) = c(i,j)
